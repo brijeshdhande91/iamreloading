891d20b4e067f55bea0ad1f477d05fa9bbe1abd5
Brijesh <brijeshdhande@yahoo.in>
2018-06-26 23:20:43 +0530

    changed
    cookieparser removed

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var express = require('express'),
 	var jwt     = require('jsonwebtoken');
 	var path = require('path');
 	var favicon = require('serve-favicon');
-	var cookieParser = require('cookie-parser');
+	//var cookieParser = require('cookie-parser');
 	var bodyParser = require('body-parser');
 
 	var routes = require('./routes/index');
@@ -61,7 +61,7 @@ var express = require('express'),
 	app.use(morgan('dev'));
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: false }));
-	app.use(cookieParser());
+	//app.use(cookieParser());
 	app.use(express.static(path.join(__dirname, 'public')));
 	app.use(session({secret: 'porfolioSecret', proxy: true, resave: true, saveUninitialized: true}));
 	/* try putting above and see session expired or not -->   ephemeral: true   */
